b8f473f5b84f6ef804b1b008c078917aa350aef2
Pandiri Likhita <pandirilikhita@aesl.in>
2026-08-01 15:47:40 +0530

    main file added

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    int ageNonStatic = 20;
+    static int ageStatic=20;
+
+    System.out.println(ageStatic);
+
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+        int age=22;
+        String name="Likhita";
+        char gender='F';
+        boolean student = false;
+        double ageDouble= age * 2;
+        System.out.println(age);
+        System.out.println(ageDouble);
+        System.out.println(student);
+        System.out.println(name);
+        System.out.println(ageStatic);
+
+
+    }
+
+
+}
