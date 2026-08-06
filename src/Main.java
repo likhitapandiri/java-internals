4497be1bc07566385c80fb9f0ef9b44d3a5c98b4
Pandiri Likhita <pandirilikhita@aesl.in>
2026-08-06 16:18:56 +0530

    Main.java refractored

--- src/Main.java
+++ src/Main.java
@@ -6,40 +6,6 @@ import Ecommerce.Product.Product;
 public class Main {
 
     public static void main(String[] args) {
-        Product laptop = new Product(101,"Laptop",70000,5);
-        laptop.setPrice(-1000);
-        laptop.setPrice(50000);
-        System.out.println(laptop.getPrice());
-
-        Customer customer = new Customer(1,"Likhita","likhita@gmail.com");
-        Payment payment = new Payment(
-                1001,
-                70000
-        );
-
-        Order order = new Order(
-                1001,
-                laptop,customer,2
-        );
-
-        payment.processPayment();
-        laptop.display();
-        customer.display();
-        order.display();
-
-
-        Payment card = PaymentFactory.getPaymentType("CARD");
-        Payment upi = PaymentFactory.getPaymentType("UPI");
-        Payment wallet = PaymentFactory.getPaymentType("WALLET");
-//        Payment randomPayment = PaymentFactory.getPaymentType("RANDOM");
-
-        card.processPayment();
-        System.out.println();
-
-        upi.processPayment();
-        System.out.println();
-
-        wallet.processPayment();
 
     }
 
@@ -64,6 +30,81 @@ public class Main {
         System.out.println(s.removeSpacesEnd(st));
         System.out.println(s.removeSpaces(st));
         System.out.println(s.compressCharacters(st));
+    }
+
+    public static void encapsulation(){
+        Product laptop = new Product(101,"Laptop",70000,5);
+        laptop.setPrice(-1000);
+        laptop.setPrice(50000);
+        System.out.println(laptop.getPrice());
+
+        Customer customer = new Customer(1,"Likhita","likhita@gmail.com");
+        Payment payment = new Payment(
+                1001,
+                70000
+        );
+
+        Order order = new Order(
+                1001,
+                laptop,customer,2
+        );
+
+        payment.processPayment();
+        laptop.display();
+        customer.display();
+        order.display();
+
+    }
+
+    public static void inheritance(){
+        CardPayment card = new CardPayment(
+                101,
+                2500,
+                "1234-5678-9012",
+                "HDFC"
+        );
+
+        UPIPayment upi = new UPIPayment(
+                102,
+                1500,
+                "likhita@upi"
+        );
+
+        WalletPayment wallet = new WalletPayment(
+                103,
+                800,
+                "Paytm"
+        );
+
+        card.processPayment();
+        System.out.println();
+
+        upi.processPayment();
+        System.out.println();
+
+        wallet.processPayment();
+
+
+    }
+
+    public static void polymorphism(){
+        try{
+
+            Payment card = PaymentFactory.getPaymentType("CARD");
+            Payment upi = PaymentFactory.getPaymentType("UPI");
+            Payment wallet = PaymentFactory.getPaymentType("WALLET");
+        Payment randomPayment = PaymentFactory.getPaymentType("RANDOM");
+
+            card.processPayment();
+            System.out.println();
+
+            upi.processPayment();
+            System.out.println();
+
+            wallet.processPayment();
+        }catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 
