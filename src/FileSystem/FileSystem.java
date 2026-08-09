c69266d8d445bb6e8740888df94647560cfffce1
Pandiri Likhita <pandirilikhita@aesl.in>
2026-08-09 17:17:19 +0530

    Wraps fileReader in BufferedReader

--- src/FileSystem/FileSystem.java
+++ src/FileSystem/FileSystem.java
@@ -1,5 +1,6 @@
 package FileSystem;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -9,18 +10,32 @@ public class FileSystem {
         try {
             //Reader - "Character-reading abstraction."
             //FileReader - "Character reader specifically for files.one of the impl of Reader
-            Reader f = new FileReader("FileSystem/input.txt"); //fileName gets checked in the current working directory not on the class package
-            int data =f.read(); //reads one character
+            Reader reader = new FileReader("FileSystem/input.txt"); //fileName gets checked in the current working directory not on the class package
+            int data =reader.read(); //reads one character
             System.out.println(data);
             System.out.println((char) data);
 
-            while((data = f.read()) != -1){
+            while((data = reader.read()) != -1){
                 System.out.println((char) data);
             }
-            f.close();
+            reader.close();
         }catch(IOException e){
             System.out.println(e.getMessage());
         }
 
+    }
+
+    public void bufferReader(){
+        try {
+            Reader reader = new FileReader("FileSystem/input.txt");
+            BufferedReader br = new BufferedReader(reader);
+
+            String line;
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
